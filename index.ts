2211ef261e54d0dc728e85240fcd94a6d84b22e5
Murray O'Brien <61970823+murrayobrienIT@users.noreply.github.com>
2022-09-11 12:26:14 +0200

    cron job

--- index.ts
+++ index.ts
@@ -27,7 +27,7 @@ fs.initializeApp({
 });
 
 //CRON JOB
-initScheduledJobs();
+// initScheduledJobs();
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
--- src/core/cron-jobs.ts
+++ src/core/cron-jobs.ts
@@ -2,9 +2,11 @@ import CronJob from "node-cron";
 import { createWordOfTheDay } from "../services/fetch-random-word";
 
 export const initScheduledJobs = async () => {
-  const scheduledJobFunction = CronJob.schedule("0 6 * * *", () => {
+  const scheduledJobFunction = CronJob.schedule("* * * * *", () => {
     createWordOfTheDay();
   });
 
   scheduledJobFunction.start();
 };
+
+initScheduledJobs();
