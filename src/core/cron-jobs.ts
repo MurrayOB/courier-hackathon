5f54a8ef909fb491d58d7aceffc32f6530fc91ab
Murray O'Brien <61970823+murrayobrienIT@users.noreply.github.com>
2022-09-11 13:04:26 +0200

    Update cron time

--- src/core/cron-jobs.ts
+++ src/core/cron-jobs.ts
@@ -2,7 +2,7 @@ import CronJob from "node-cron";
 import { createWordOfTheDay } from "../services/fetch-random-word";
 
 export const initScheduledJobs = async () => {
-  const scheduledJobFunction = CronJob.schedule("0 6 * * *", () => {
+  const scheduledJobFunction = CronJob.schedule("45 5 * * *", () => {
     createWordOfTheDay();
   });
 
