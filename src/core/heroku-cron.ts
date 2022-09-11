89cb858f2f990be4224c6d9d768645e702e43d45
Murray O'Brien <61970823+murrayobrienIT@users.noreply.github.com>
2022-09-11 16:43:39 +0200

    Update cron

--- src/core/heroku-cron.ts
+++ src/core/heroku-cron.ts
@@ -1,3 +1,10 @@
 import { createWordOfTheDay } from "../services/fetch-random-word";
+import { getFirebaseCredentials } from "./firebase";
+import fs, { ServiceAccount } from "firebase-admin";
+
+const firebaseCredentials = getFirebaseCredentials();
+fs.initializeApp({
+  credential: fs.credential.cert(<ServiceAccount>firebaseCredentials),
+});
 
 createWordOfTheDay();
