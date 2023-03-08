a450b510acf31927db4723b72c8cefe0e017de5b
Murray <murrayobrien.dev@gmail.com>
2023-03-08 10:25:51 +0800

    Add response

--- src/controllers/cron.ts
+++ src/controllers/cron.ts
@@ -3,4 +3,5 @@ import { createWordOfTheDay } from "../services/fetch-random-word";
 
 export const cron = async (req: Request, res: Response) => {
   createWordOfTheDay();
+  return res.status(202).json({ success: true, message: "" });
 };
