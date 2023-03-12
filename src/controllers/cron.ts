a2f39859bd7d9a4834a1131dfdef270e54a8369c
Murray <murrayobrien.dev@gmail.com>
2023-03-12 17:45:15 +0800

    Add await

--- src/controllers/cron.ts
+++ src/controllers/cron.ts
@@ -2,6 +2,6 @@ import { Request, Response } from "express";
 import { createWordOfTheDay } from "../services/fetch-random-word";
 
 export const cron = async (req: Request, res: Response) => {
-  createWordOfTheDay();
+  await createWordOfTheDay();
   return res.status(202).json({ success: true, message: "" });
 };
